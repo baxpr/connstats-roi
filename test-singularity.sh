75f77dacbb8472ca474525d77d5feb0f320a2674
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2023-11-28 11:55:16 -0600

    testcode

--- test-singularity.sh
+++ test-singularity.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-singularity exec --cleanenv --contain \
+singularity run --cleanenv --contain \
     --bind $(pwd -P)/INPUTS:/INPUTS \
     --bind $(pwd -P)/OUTPUTS:/OUTPUTS \
     /data/mcr/centos7/singularity/connstats-roi_v1.0.1.sif \
