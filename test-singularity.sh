4572f3f89a7d46d5ae3fa05eda6e732dc7990ea4
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2023-11-28 11:53:23 -0600

    testcode

--- test-singularity.sh
+++ test-singularity.sh
@@ -5,5 +5,6 @@ singularity exec --cleanenv --contain \
     --bind $(pwd -P)/INPUTS:/INPUTS \
     --bind $(pwd -P)/OUTPUTS:/OUTPUTS \
     /data/mcr/centos7/singularity/connstats-roi_v1.0.1.sif \
-    bash
-
+    matrix_csv /INPUTS/Z_removegm.csv
+    out_dir /OUTPUTS
+    seed_roi DMN_LatPar_L
